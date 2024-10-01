43781269ffd5a494a19614394688dffc90ca9dde
KaratasKTS <nikoskaratas@gmail.com>
2024-10-01 23:31:22 +0300

    completed the logic, need to fix some bugs though, building a ui and connecting to the dom

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
+const boxes = document.querySelectorAll('.box').forEach( box => box.addEventListener('click', handleEvent, evalBoard));
+
+// IIFE HOLDING PRIVATE OBJECTS AND METHODS  
 let init = (() => {
-    let score = { playerOne: 0 , playerTwo : 0};
+    let score = { playerOne: true  , playerTwo : false };
     let gameboard = [] ; 
    
    for( let i=0; i !==3 ;  i++ ){
        gameboard[i] = [];
        for(let j = 0; j < 3; j++)
-        gameboard[i][j] = null;
+        gameboard[i][j] = '';
    
    }
-    console.log('it happent');
+    console.log(gameboard);
     
-    const incrementPlayerOne = () => score.playerOne++;
-    const incrementPlayerTwo = () => score.playerTwo++; 
-    return {score , gameboard, incrementPlayerOne, incrementPlayerTwo };
+    const truePlayerOne = () => score.playerOne = true;
+    const truePlayerTwo = () => score.playerTwo = true;
+    const falsePlayerOne = () => score.playerOne = false;
+    const falsePlayerTwo = () => score.playerTwo = false;
+    return {score , gameboard, truePlayerOne, truePlayerTwo, falsePlayerOne, falsePlayerTwo };
    
    })()
+/* gets the boxes event and target atributes check player status and edits array with either X OR O,
+NEED TO MAKE  APPEND TO DOM TOO 
+*/
+function handleEvent(event) {
+    const { score, gameboard, truePlayerOne, truePlayerTwo, falsePlayerOne, falsePlayerTwo } = init;
+    let boxed = event.target;
+    let row = boxed.getAttribute('data-row');
+    let col = boxed.getAttribute('data-col');
 
+
+    if (score.playerOne === true) {
+        
+        if (gameboard[row][col] === '') {
+            gameboard[row][col] = 'X'; 
+            falsePlayerOne(); 
+            evalBoard();
+            console.log(gameboard);
+        }
+    } else {
+        
+        if (gameboard[row][col] === '') {
+            gameboard[row][col] = 'O'; 
+            truePlayerOne(); 
+            evalBoard();
+            console.log(gameboard);
+        }
+    }
+}
+
+// WIN EVALUATOR, this counts equates epty boxes as a match, need fix 
 function evalBoard (){
     const {gameboard} = init;
 
@@ -52,6 +86,6 @@ if (gameboard[0][0] === gameboard[0][1] && gameboard[0][1] === gameboard[0][2]||
        
     
         
-   evalBoard();
+   
    
 
