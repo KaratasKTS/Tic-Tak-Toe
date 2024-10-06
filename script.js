720e0848b8f3350743236b6b1282dbe48d3bfa26
KaratasKTS <nikoskaratas@gmail.com>
2024-10-06 11:03:25 +0300

    made the scores display to the ui,just need to add some refresh buttons and make it pretty :)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const boxes = document.querySelectorAll('.box').forEach( box => box.addEventListener('click', handleEvent, evalBoard));
+const scorePlayer1 = document.querySelector('.scorePlayer1');
+const scorePlayer2 = document.querySelector('.scorePlayer2');
 
 // IIFE HOLDING PRIVATE OBJECTS AND METHODS  
 let init = (() => {
@@ -19,7 +21,7 @@ let init = (() => {
     
     return {score , gameboard, truePlayerOne,  falsePlayerOne };
    
-   })()
+   })();
 
 function handleEvent(event) {
     const { score, gameboard, truePlayerOne,  falsePlayerOne, } = init;
@@ -49,13 +51,17 @@ function handleEvent(event) {
     }
 }
 
-// WIN EVALUATOR, this counts equates epty boxes as a match, need fix 
+// WIN EVALUATOR
 function evalBoard (){
     const {gameboard} = init;
-      
-
+      const {
+        player1Wins,
+        player2Wins,
+    } = callWin;
         if (gameboard[0][0] === 'X'  &&  gameboard[0][1]=== 'X' &&  gameboard[0][2] === 'X'){
             console.log('yay X wins') ;
+            player1Wins();
+            
             } else if ( gameboard[0][0] === 'O'  &&  gameboard[0][1]=== 'O' &&  gameboard[0][2] === 'O'){
                 console.log('yay O wins') ;
             }
@@ -64,6 +70,7 @@ function evalBoard (){
                 gameboard[2][0] === 'X' && gameboard[1][1] === 'X' && gameboard[0][2] ==='X'){
                 
                     console.log('yay X wins') ;
+                    
             
                 }else if (gameboard[0][0] === 'O' &&  gameboard[1][1] === 'O' &&  gameboard[2][2] === 'O'||
                       gameboard[2][0] === 'O' && gameboard[1][1] === 'O' && gameboard[0][2] ==='O'){
@@ -74,6 +81,7 @@ function evalBoard (){
                gameboard[0][1] === 'X' && gameboard[1][1] ==='X' && gameboard[2][1]==='X'||
                gameboard[0][2] === 'X' && gameboard[1][2] === 'X' && gameboard[2][2]==='X'){
                 console.log('yay X wins') ;
+                
               
             } else if (gameboard[0][0] === 'O' && gameboard[1][0] === 'O' && gameboard[2][0] ==='O'||
                 gameboard[0][1] === 'O' && gameboard[1][1] ==='O' && gameboard[2][1]==='O'||
@@ -83,6 +91,36 @@ function evalBoard (){
               
             
             }
+
+            const callWin = (() => {
+                
+                let player1Score = 0;
+                let player2Score = 0;
+
+                const player1Wins = () => {
+                    player1Score++;
+                    updateScores();
+                };
+            
+                
+                const player2Wins = () => {
+                    player2Score++;
+                    updateScores();
+                };
+            
+               
+                const updateScores = () => {
+                    scorePlayer1.textContent = player1Score;
+                    scorePlayer2.textContent = player2Score; 
+                };
+            
+                
+                return {
+                    player1Wins,
+                    player2Wins,
+                };
+            })();
+            
 
 function cleanUp (){
 const { gameboard} = init;
@@ -96,7 +134,7 @@ const { gameboard} = init;
 }
 
     
-    cleanUp();
+    
        
     
         
